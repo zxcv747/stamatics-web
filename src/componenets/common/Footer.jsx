15657ef02ed3bfbb4b421d2934c1c8c9daf7265f
zxcv747 <sumathrengasami@gmail.com>
2025-11-30 23:43:36 +0530

    Create Footer.jsx

---
+++ src/componenets/common/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="w-full py-6 text-center text-sm bg-nav text-navText mt-10">
+      © {new Date().getFullYear()} Statamatics, IIT Kanpur. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
